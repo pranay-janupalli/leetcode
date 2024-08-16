d8bcd3d0c4cd5f94f8ecd836f3bfc332e4ce309e
pranay-janupalli <64585896+pranay-janupalli@users.noreply.github.com>
2024-08-17 00:22:36 +0530

    Update longest_palindrome.py

--- strings/longest_palindrome.py
+++ strings/longest_palindrome.py
@@ -1,1 +1,24 @@
 
+def find_longest_palindrome(s):
+    memoization = [[0]*len(s) for _ in range(len(s))]
+    for i in range(len(s)):
+        memoization[i][i]=1
+        palindrome=s[i]
+    for i in range(len(s)-1,-1,-1):
+        for j in range(i+1,len(s)):
+            if s[i]==s[j]:
+                if (j-i)>1: 
+                    if memoization[i+1][j-1]==1:
+                        memoization[i][j]=1
+                        current_palindrome=s[i:j+1]
+                        if len(current_palindrome)>len(palindrome):
+                            palindrome=current_palindrome
+                        # max_length=max(max_length,(j-i+1))
+                else:
+                    memoization[i][j]=1
+                    if len(current_palindrome)>len(palindrome):
+                        palindrome=current_palindrome
+    return palindrome
+
+s="abcacbj"
+print(find_longest_palindrome(s)) #bcacb
